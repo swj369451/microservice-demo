afe4897e0e56abc7f169bc4a4b5468e9de113708
aslin23 <2327920702@qq.com>
2022-05-04 23:48:16 +0800

    注册了一个服务

---
+++ microservice-demo-provider/microservice-demo-provider-service/src/main/java/com/example/microservicedemoproviderservice/MicroserviceDemoProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicedemoproviderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDemoProviderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDemoProviderServiceApplication.class, args);
+    }
+
+}
